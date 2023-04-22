9e09d5fa0d74b73137435e2fcd6daeb8b46fa58b
George Cojocariu <georgecojocariu1@gmail.com>
2023-04-22 12:34:58 +0200

    DB config and connect

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,8 @@ import java.util.logging.Logger;
 
 public class Main {
 
-    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private final
+    static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         String dbLocation = "localhost:3306";
