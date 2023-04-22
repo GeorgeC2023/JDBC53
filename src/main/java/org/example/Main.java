d165dd9009cfba0f2178b5c9365932c3b707ca70
George Cojocariu <georgecojocariu1@gmail.com>
2023-04-22 12:26:48 +0200

    add database configuration and connect to db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 public class Main {
 
-    private static Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         String dbLocation = "localhost:3306";
